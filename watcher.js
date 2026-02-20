87f387c7583309eab483df9d707ba0bad0fc4827
Aziz Ahmed <aziz.ahmed@urbanstems.com>
2026-02-19 20:24:52 -0600

    Initial commits

---
+++ watcher.js
@@ -0,0 +1,15 @@
+const zmq = require("zeromq");
+const socket = zmq.socket("sub");
+
+socket.connect("tcp://127.0.0.1:5555");
+
+// should match the topic name configured in config-nile.conf
+socket.subscribe("transaction");
+
+console.log("Subscribed to transaction events on tcp://127.0.0.1:5555");
+
+socket.on("message", (topic, message) => {
+  console.log("topic:", topic.toString());
+  console.log("msg:", message.toString());
+  console.log("----");
+});
